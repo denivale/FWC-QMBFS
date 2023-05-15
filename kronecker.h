b03d28fc1f3aec73368bad07c21fcbedb5d49557
denivale <denivalesq@gmail.com>
2023-05-15 18:44:06 +0200

    Add files via upload
    Removing unimportant comments

--- kronecker.h
+++ kronecker.h
@@ -1,3 +1,13 @@
+//========================================================================
+/*
+D. Vale, N. Paar 
+ - created Feb 01 2019
+ - last modification May 15 2023 (D. Vale)
+*/
+//========================================================================
+
+
+
 #ifndef _KRONECKER_H
 #define _KRONECKER_H
 
@@ -5,12 +15,10 @@
 typedef struct{
 char ind1[INDEX_SIZE];
 char ind2[INDEX_SIZE];
-short pos1;//-1 -> pozicija me ne zanima
+short pos1;
 short pos2;
-//short perm = -1;//informacija o permutaciji (bitno za crtanje)
-//short comb = -1;//informacija o kombinaciji -1 -> ne zanima me
-short rez;//0,1 Pretpostavimo i nulu kao mogucnost
-short fermi;//-1 ispod, 1 iznad Fermijevog, 0 sve mogucnosti (za neki generalni operator iako me trenutno to ne interesira. Ipak ostavljam kao jednu od mogucnosti)
+short rez;
+short fermi;
 } kronecker_t;
 
 typedef struct{
@@ -26,35 +34,31 @@ predfac_t predfac;
 typedef struct{
 kronecker_t **delta;
 short comb;
-} kronecker_c_t; //kombinacije
+} kronecker_c_t; 
 
 
-//stvaranje polja Kroneckerovih simbola
+
 kronecker_t * create_field_kronecker(int);
 kronecker_t ** create_2D_field_kronecker(int, int);
 all_kronecker_t create_empty_all_kronecker(int n);
 
-//kopiranje polja Kroneckerovih simbola
-//kronecker_t * copy_kronecker_field(kronecker_t *, int);
+
 void copy_kronecker_field(kronecker_t *, kronecker_t *, int);
 void copy_kronecker_deltas(kronecker_t * dest, kronecker_t * source, int n);
 void copy_predfac(predfac_t * temp, predfac_t source);
 void copy_all_kronecker_deltas(all_kronecker_t *temp, all_kronecker_t source);
 
-//fill kronecker
 void fill_kronecker(kronecker_t *, char *, char *, short, short, short, short);
 
-//print kronecker
+
 void print_kronecker_standard(kronecker_t);
 void print_kroencker1D_standard(kronecker_t *, short, int);
 void print_all_kronecker_deltas(all_kronecker_t delta);
 
-//brisanje polja Kroneckerovih simbola
 void kronecker_free(kronecker_t *);
 void kronecker_2D_free(kronecker_t **);
 void kronecker_all_free(all_kronecker_t *all);
 
-//testovi
 void test_kronecker();
 
 #endif
