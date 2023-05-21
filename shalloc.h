819b09b2ebfdebbc4f82c0ef3f92124bc59d2b02
denivale <denivalesq@gmail.com>
2023-05-21 16:17:40 +0200

    Add files via upload

--- shalloc.h
+++ shalloc.h
@@ -1,17 +1,24 @@
+//========================================================================
+/*
+D. Vale, N. Paar 
+ - created Jan 25 2019
+ - last modification May 21 2023 (D. Vale)
+*/
+//========================================================================
 #ifndef _SHALLOC_H
 #define _SHALLOC_H
 
 typedef struct{
 short * v;
 int n;
-} _1D_short_t;
+} _1D_short_t;//vector (1d array)
 
 short * _1Ds(int);
 short ** _2Ds(int, int);
 short ** _2Ds_adjustable(int, int *);
 void test_shalloc();
 void E1Ds(short *);
-void E2Ds(short **);//neovisno o tipu
+void E2Ds(short **);
 
 
 #endif
